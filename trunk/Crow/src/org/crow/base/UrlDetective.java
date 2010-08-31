9a63133c89dc975d869912779bcfe595e2dc6229
vikasing <vikasing@bc0d78c5-dd84-4c63-3841-c1391d2082cf>
2010-08-31 19:10:00 +0000

    changes for robots.txt

--- trunk/Crow/src/org/crow/base/UrlDetective.java
+++ trunk/Crow/src/org/crow/base/UrlDetective.java
@@ -7,8 +7,10 @@ package org.crow.base;
  * @author viksin
  * This class tries to determine the type of a url i.e. if it is feed url or some other type.
  */
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -49,7 +51,7 @@ public class UrlDetective {
 				//DefaultHttpClient httpClient = new DefaultHttpClient();
 				//HttpGet pageGet = new HttpGet(feedUrl.toURI());
 				//HttpResponse response = httpClient.execute(pageGet);
-				analyzeRobotDotText(passedUrl);
+				analyzeRobotsDotText(passedUrl);
 				URLConnection conn = passedUrl.openConnection();
 				if(conn instanceof HttpURLConnection)
 				{
@@ -83,62 +85,21 @@ public class UrlDetective {
 			
 		//}
 	}
-	private boolean analyzeRobotDotText(URL url) {
+	private void analyzeRobotsDotText(URL url) {
 		String strHost = url.getHost();
 
-		// form URL of the robots.txt file
 		String strRobot = "http://" + strHost + "/robots.txt";
 		URL urlRobot;
 		try { 
 		    urlRobot = new URL(strRobot);
-		} catch (MalformedURLException e) {
-		    // something weird is happening, so don't trust it
-		    return false;
-		}
-
-		String strCommands;
-		try {
 		    InputStream urlRobotStream = urlRobot.openStream();
-
-		    // read in entire file
-		    byte b[] = new byte[1000];
-		    int numRead = urlRobotStream.read(b);
-		    strCommands = new String(b, 0, numRead);
-		    while (numRead != -1) {
-			if (Thread.currentThread() != searchThread)
-			    break;
-			numRead = urlRobotStream.read(b);
-			if (numRead != -1) {
-			    String newCommands = new String(b, 0, numRead);
-			    strCommands += newCommands;
-			}
-		    }
-		    urlRobotStream.close();
-		} catch (IOException e) {
-		    // if there is no robots.txt file, it is OK to search
-		    return true;
-		}
-
-		// assume that this robots.txt refers to us and 
-		// search for "Disallow:" commands.
-		String strURL = strRobot;
-		int index = 0;
-		while ((index = strCommands.indexOf(DISALLOW, index)) != -1) {
-		    index += DISALLOW.length();
-		    String strPath = strCommands.substring(index);
-		    StringTokenizer st = new StringTokenizer(strPath);
-
-		    if (!st.hasMoreTokens())
-			break;
+		    BufferedReader br = new BufferedReader(new InputStreamReader(urlRobotStream));
 		    
-		    String strBadPath = st.nextToken();
-
-		    // if the URL starts with a disallowed path, it is not safe
-		    if (strURL.indexOf(strBadPath) == 0)
-			return false;
+		} catch (Exception e) {
+		    //return true;
 		}
 
-		return true;
+		
 	    }
 	
 }
