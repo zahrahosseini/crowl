9d6f4e454fd0acf1fa22120790826f4a3c46a3ca
vikasing <vikasing@bc0d78c5-dd84-4c63-3841-c1391d2082cf>
2010-11-20 01:38:46 +0000

    added url shortener ==> base62converter

--- trunk/Crow/src/org/crow/utils/Base62Converter.java
+++ trunk/Crow/src/org/crow/utils/Base62Converter.java
@@ -8,10 +8,16 @@ package org.crow.utils;
  *
  */
 public class Base62Converter {
-	
+	private final int LENGTH_OF_URL_CODE=6;
 	public String convertTo62Base(long toBeConverted)
 	{
-		String[] elements = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","1","2","3","4","5","6","7","8","9","0","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+		String[] elements = {
+				"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o",
+				"p","q","r","s","t","u","v","w","x","y","z","1","2","3","4",
+				"5","6","7","8","9","0","A","B","C","D","E","F","G","H","I",
+				"J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X",
+				"Y","Z"
+				};
 		String convertedString="";
 		if(toBeConverted<63 && toBeConverted>0)
 		{
@@ -19,8 +25,32 @@ public class Base62Converter {
 		}
 		else if(toBeConverted>62)
 		{
+			long mod = 0;
+			long multiplier = 0;
+			boolean determinedTheLength=false;
+			for(int j=LENGTH_OF_URL_CODE;j>=0;j--)
+			{
+				multiplier=(long) (toBeConverted/Math.pow(62,j));
+				if(multiplier>0 && toBeConverted>=62)
+				{
+					convertedString+=elements[(int) multiplier];
+					determinedTheLength=true;
+				}
+				else if(determinedTheLength && multiplier==0)
+				{
+					convertedString+=elements[0];
+				}
+				else if(toBeConverted<62)
+				{
+					convertedString+=elements[(int) mod];
+				}
+				
+				mod=(long) (toBeConverted%Math.pow(62,j));
+				toBeConverted=mod;				
+			}
 			
 		}
+		System.out.print(convertedString);
 		return convertedString;
 	}
 
